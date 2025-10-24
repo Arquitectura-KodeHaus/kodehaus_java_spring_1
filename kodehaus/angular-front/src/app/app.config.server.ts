fa0fb65d84ccd67a9beaa4adf1361cae832221c1
Sebastian Rodriguez <140280819+T3cm0@users.noreply.github.com>
2025-10-24 11:03:37 -0500

    Delete kodehaus directory

--- kodehaus/angular-front/src/app/app.config.server.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationConfig, mergeApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
-import { appConfig as clientConfig } from './app.config';
-
-const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering()],
-};
-
-export const appConfig = mergeApplicationConfig(clientConfig, serverConfig);
